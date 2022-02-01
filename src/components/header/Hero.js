252babbd625768068ba156ee00db6497793ca852
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-01 14:30:04 -0500

    Fix button padding

--- src/components/header/Hero.js
+++ src/components/header/Hero.js
@@ -1,18 +1,21 @@
 import React from "react";
-import styles from "./Hero.module.css"
+import styles from "./Hero.module.css";
 import ButtonCTA from "../UI/ButtonCTA";
-import illustration from "../../assets/illustration-working.svg"
+import illustration from "../../assets/illustration-working.svg";
 function Hero() {
-    return (
-        <section className={styles.hero}>
-            <article className={styles["hero__article"]}>
-                <h1>More than just shorter links</h1>
-                <p>Build your brand’s recognition and get detailed insights on how your links are performing.</p>
-                <ButtonCTA text="Get Started"/>
-            </article>
-            <img className={styles["hero__img"]} src={illustration} />
-        </section>
-    )
+  return (
+    <section className={styles.hero}>
+      <article className={styles["hero__article"]}>
+        <h1 className={styles["hero__h1"]}>More than just shorter links</h1>
+        <p className={styles["hero__p"]}>
+          Build your brand’s recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <ButtonCTA text="Get Started" />
+      </article>
+      <img className={styles["hero__img"]} src={illustration} />
+    </section>
+  );
 }
 
 export default Hero;
