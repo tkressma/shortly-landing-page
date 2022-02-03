e2dd2b566f2f58648254b87e031fc91f607254e5
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-02 21:21:57 -0500

    Update feature card text

--- src/components/main/Features/FeatureCards.js
+++ src/components/main/Features/FeatureCards.js
@@ -14,16 +14,16 @@ function FeatureCards() {
         description="Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content."
       />
       <FeatureCard
-        title="Brand Recognition"
-        icon={brandRecognitionIcon}
-        altText="A graph showing an increasing trend"
-        description="Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content."
+        title="Detailed Records"
+        icon={detailedRecordsIcon}
+        altText="A meter to keep track of some data."
+        description="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
       />
       <FeatureCard
-        title="Brand Recognition"
-        icon={brandRecognitionIcon}
-        altText="A graph showing an increasing trend"
-        description="Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content."
+        title="Fully Customizable"
+        icon={fullyCustomizableIcon}
+        altText="A marker, an ink pen, and a brush to signify full creative control."
+        description="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
       />
     </div>
   );
