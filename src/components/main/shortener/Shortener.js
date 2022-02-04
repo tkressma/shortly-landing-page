07e671eb7118300cee084146accb2ac1cf0422e2
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-03 23:39:08 -0500

    Add error styling to input form

--- src/components/main/shortener/Shortener.js
+++ src/components/main/shortener/Shortener.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import styles from "./Shortener.module.css";
+import classNames from "classnames";
 import Button from "../../UI/Button";
 import validator from "validator";
 import { isValidDateValue } from "@testing-library/user-event/dist/utils";
 
 function Shortener(props) {
   const [inputLink, setInputLink] = useState("");
-  const [errorMsg, setErrorMsg] = useState("");
   const [isValid, setIsValid] = useState(true);
 
   const shortenHandler = (event) => {
     event.preventDefault();
 
     if (validator.isURL(inputLink)) {
+      setIsValid(true);
       const linkData = {
         inputLink: inputLink,
         shortLink: inputLink,
@@ -32,17 +33,23 @@ function Shortener(props) {
     <section className={styles.shortener}>
       <form onSubmit={shortenHandler}>
         <input
-          className={styles["shortener__input"]}
+          className={classNames(
+            styles["shortener__input"],
+            !isValid && styles.error
+          )}
           id="input_link"
           type="text"
           placeholder="Shorten a link here..."
           value={inputLink}
           onChange={linkChangeHandler}
-          valid={isValid}
-          errorText={errorMsg}
         />
         <Button type="submit" text="Shorten it!" />
       </form>
+      {!isValid && (
+        <label className={styles["shortener__error"]}>
+          Please add a valid link
+        </label>
+      )}
     </section>
   );
 }
