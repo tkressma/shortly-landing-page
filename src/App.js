d67a04a82f2e417258399e945d5034b8c6303727
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-04 13:45:10 -0500

    Add footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <React.Fragment>
       <Header />
       <Main />
+      <Footer />
     </React.Fragment>
   );
 }
